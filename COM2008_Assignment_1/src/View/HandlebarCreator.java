327ad7fd9c5b378939d3c770039ec57b64007514
Adam Jenkins <atjenkins1@sheffield.ac.uk>
2022-11-04 10:23:28 +0000

    Added Handlebar Attributes to GUI Handlebar Designer page

--- COM2008_Assignment_1/src/View/HandlebarCreator.java
+++ COM2008_Assignment_1/src/View/HandlebarCreator.java
@@ -7,7 +7,9 @@ import java.awt.Frame;
 
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 import Domain.Bicycle;
@@ -21,6 +23,7 @@ public class HandlebarCreator extends JDialog {
 	}
 	
 	private void addComponents() {
+		//Serial Number GUI Input Fields:
 		JLabel serialNumberLabel = new JLabel("Serial Number: ");
 		
 		JTextField serialNumberField = new JTextField();
@@ -30,26 +33,53 @@ public class HandlebarCreator extends JDialog {
 		serialNumberRow.add(serialNumberLabel);
 		serialNumberRow.add(serialNumberField);
 		
-		JLabel serialNumberLabel2 = new JLabel("Serial Number: ");
+		//Brand Name GUI Input Fields:
+		JLabel brandNameLabel = new JLabel("Brand Name: ");
 		
-		JTextField serialNumberField2 = new JTextField();
-		serialNumberField2.setPreferredSize(new Dimension(200, 20));
+		JTextField brandNameField = new JTextField();
+		brandNameField.setPreferredSize(new Dimension(200, 20));
 		
-		JPanel serialNumberRow2 = new JPanel(new FlowLayout());	
-		serialNumberRow2.add(serialNumberLabel2);
-		serialNumberRow2.add(serialNumberField2);
+		JPanel brandNameRow = new JPanel(new FlowLayout());	
+		brandNameRow.add(brandNameLabel);
+		brandNameRow.add(brandNameField);
 		
+		//Cost GUI Input Fields:
+		JLabel costLabel = new JLabel("Brand Name: ");
 		
+		JTextField costField = new JTextField();
+		costField.setPreferredSize(new Dimension(200, 20));
+		
+		JPanel costRow = new JPanel(new FlowLayout());	
+		costRow.add(costLabel);
+		costRow.add(costField);
+		
+		//Style GUI Input Fields:
+		JLabel stylesLabel = new JLabel("Styles: ");
+		
+		String [] styles = new String [] {"high", "straight", "dropped"};
+		
+		JList styleList = new JList(styles);
+		styleList.setVisibleRowCount(3);
+		JScrollPane styleField = new JScrollPane(styleList);
+		styleField.setPreferredSize(new Dimension(200, 20));
+		
+		JPanel styleRow = new JPanel(new FlowLayout());
+		styleRow.add(stylesLabel);
+		styleRow.add(styleField);
+		
+		//Set GUI Layout:
 		final Container pane = this.getContentPane();
 		FlowLayout topToBot = new FlowLayout();
 		topToBot.setAlignment(FlowLayout.LEADING);
 		
+		//Add Created GUI Fields to the pane:
 		pane.setLayout(topToBot);
 		pane.add(serialNumberRow);
-		pane.add(serialNumberRow2);
+		pane.add(brandNameRow);
+		pane.add(costRow);
+		pane.add(styleRow);
 		
 		//TODO:
-		//ADD REST OF ATTRIBUTES OF HANDLEBAR TO UI
 		//ADD OK BUTTON THAT CALLS THE createHandlebar() DATABASE METHOD AFTER VALIDATING INPUT
 	}
 	
