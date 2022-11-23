7c46937e400ac6fad1be67313ac428b3870eba63
TomPearson38 <tomapearson38@gmail.com>
2022-11-23 16:52:29 +0000

    Forgot to add this to previous push

--- COM2008_Assignment_1/src/SQL/data/Staff.sql
+++ COM2008_Assignment_1/src/SQL/data/Staff.sql
@@ -2,6 +2,6 @@ USE team028;
 
 CREATE TABLE Staff (
     username VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(20) NOT NULL,
+    password VARCHAR(64) NOT NULL,
     PRIMARY KEY (username)
 );
