e5478af89532809b9b0e3868a7a60bfe21dded6e
alexdobson <alexdobsonpleming@gmail.com>
2022-11-25 15:17:24 +0000

    Added failover type infererence

--- COM2008_Assignment_1/src/View/StaffWindow/Column.java
+++ COM2008_Assignment_1/src/View/StaffWindow/Column.java
@@ -1,5 +1,6 @@
 package View.StaffWindow;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.function.Function;
 
 /*
@@ -10,11 +11,23 @@ class Column<O, T> {
 	private Function<O, T> getValueFromObject;
 	private Class<T> underlyingType;
 	
+	public Column(String name, Function<O, T> getValueFromObject) {
+		this(name, getValueFromObject, null);	//have to put null here as can't call instance methods (getClass()) in an intra-constructor call
+	}
+	
+	@SuppressWarnings("unchecked")	//only a failover in the event that a type isn't provided
 	public Column(String name, Function<O, T> getValueFromObject, Class<T> type) {
 		super();
 		this.name = name;
 		this.getValueFromObject = getValueFromObject;
-		this.underlyingType = type;
+		
+		if (type != null) {
+			this.underlyingType = type;
+		} else {
+			this.underlyingType = (Class<T>) ((ParameterizedType) getClass()
+	                .getGenericSuperclass()).getActualTypeArguments()[0];
+		}
+		
 	}
 	
 	public String getName() {
