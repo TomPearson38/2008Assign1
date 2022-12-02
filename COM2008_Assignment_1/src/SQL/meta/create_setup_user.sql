5d76511607e9a386be451f78f82ddeff4dbc2e2a
alexdobson <alexdobsonpleming@gmail.com>
2022-12-02 11:55:58 +0000

    Expunged old files for creating tables on a local database

--- COM2008_Assignment_1/src/SQL/meta/create_setup_user.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'setup'@'localhost';
-
-GRANT ALL PRIVILEGES ON `%`.* TO setup@localhost; 
-
-UPDATE mysql.user SET Grant_priv='Y' WHERE User='setup';
-
-FLUSH PRIVILEGES;
--- COM2008_Assignment_1/src/SQL/meta/setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE buildabike;
-
-CREATE USER 'client'@'%' IDENTIFIED BY 'password';
-
-GRANT INSERT, UPDATE, DELETE, SELECT ON buildabike.* TO 'client'@'%';
